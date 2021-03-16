4b68411948392ab96f0348f147ed2a317050b443
alejandrodp <dpereiraalejandro@gmail.com>
2021-03-16 03:56:05 -0600

    UPDATE

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import webhook
 
 urlpatterns = [
-    path('<str:secret>', webhook)
+    path('<str:secret>', csrf_exempt(webhook))
 ]
